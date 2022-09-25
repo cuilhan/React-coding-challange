3c9729d0309d557b940a8ebc48cf0279d92387de
cuilhan <ilhanumit@gmail.com>
2022-09-25 14:21:39 -0400

    integration with tailwind css

--- nextjs-challenge/pages/index.js
+++ nextjs-challenge/pages/index.js
@@ -1,32 +1,34 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Tech Guilds Next.js Challenge</title>
         <meta name="description" content="Challenge for interview" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="main">
+        <h1 className="title">
           Unsplash Image Gallery
         </h1>
-
-        
+        <div className="image-gallery">
+          <h2 className="sub-title">
+              Image gallery will be here!
+          </h2>
+        </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={"footer"}>
         <a
           href="https://www.techguilds.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Created for{' '}
-          <span className={styles.logo}>
+          <span className={"logo"}>
             <Image src="/techguilds-logo.webp" alt="Techguilds Logo" width={120} height={21} />
           </span>
         </a>
